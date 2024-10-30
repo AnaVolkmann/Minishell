c86001e62fa5fc289e38d2a0aad061104555f913
Lucas Figueiredo Silva Graca <lufiguei@c2r1s2.42porto.com>
2024-10-30 15:45:32 +0000

    Added suppression

---
+++ main.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lufiguei <lufiguei@student.42porto.co      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/30 15:08:55 by lufiguei          #+#    #+#             */
+/*   Updated: 2024/10/30 15:12:13 by lufiguei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <stdlib.h>
+
+int	main()
+{
+	char *rl;
+	rl = readline("Prompt > ");
+	printf("%s\n", rl);
+	free(rl);
+	rl_clear_history();
+	return (0);
+}
