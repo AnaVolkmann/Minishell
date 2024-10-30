ee3925f109d194c34537cbef294ae19a9ae85699
Lucas Figueiredo Silva Graca <lufiguei@c2r1s2.42porto.com>
2024-10-30 16:04:21 +0000

    added header file

---
+++ minishell.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lufiguei <lufiguei@student.42porto.co      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/30 15:56:04 by lufiguei          #+#    #+#             */
+/*   Updated: 2024/10/30 16:03:58 by lufiguei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <dirent.h>
+# include <fcntl.h>
+# include <signal.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <stropts.h>
+# include <sys/types>
+# include <sys/stat>
+# include <sys/wait>
+# include <term.h>
+# include <termios.h>
+# include <unistd.h>
+# include <readline/history>
+# include <readline/readline>
+
+#endif
