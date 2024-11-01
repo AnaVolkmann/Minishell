ed0f8dda760eca69bb5bd8820893e5053e967e90
Ana Laura Da Silva Volkmann <ana-lda-@c2r1s2.42porto.com>
2024-11-01 13:52:54 +0000

    makefile added

--- Makefile
+++ Makefile
@@ -3,33 +3,50 @@ RM = rm -rf
 CFLAGS = -Wall -Werror -Wextra -g
 
 NAME = minishell
-
 LIBFT = libft/libft.a
 
-SRC = main.c\
-	parser.c\
-	
-OBJ = $(SRC:.c=.o)
+OBJ_DIR = obj/
+SRC = main.c \
+      parser.c
 
-all: $(LIBFT) $(NAME)
+OBJ = $(addprefix $(OBJ_DIR), $(SRC:.c=.o))
 
+# Default target
+all: $(NAME)
+
+# Create object directory
+$(OBJ_DIR):
+	mkdir -p $(OBJ_DIR)
+
+# Build libft
 $(LIBFT):
-	$(MAKE) -C libft
+	@$(MAKE) -C libft > /dev/null 2>&1
 
+# Link the final executable
 $(NAME): $(OBJ) $(LIBFT)
-	$(CC) $(CFLAGS) $(OBJ) -o $(NAME) $(LIBFT)
+	$(CC) $(CFLAGS) $(OBJ) -s -o $(NAME) $(LIBFT) -lreadline > /dev/null 2>&1
+	@printf "\r \e[1;32mMINISHELL\e[0m compiled successfully\n"
 
-%.o: %.c
-	$(CC) $(CFLAGS) -c $< -o $@
+# Compile source files into object files
+$(OBJ_DIR)%.o: %.c | $(OBJ_DIR)
+	@$(CC) $(CFLAGS) -c $< -o $@ > /dev/null 2>&1
 
+# Clean up object files
 clean:
-	$(RM) $(OBJ)
-	$(MAKE) -C libft clean
+	@$(RM) $(OBJ)
+	@$(MAKE) -C libft clean > /dev/null 2>&1
+	@printf " \e[1;33mObjects Removed\n\e[0m"
 
+# Fully clean including the executable
 fclean: clean
-	$(RM) $(NAME)
-	$(MAKE) -C libft fclean
+	@$(RM) $(NAME)
+	@$(MAKE) -C libft fclean > /dev/null 2>&1
+	@printf " \e[1;31mExecutable Deleted\n\e[0m"
 
+# Rebuild everything
 re: fclean all
+	@printf "\r \e[1;32mRefresh OK!\e[0m\n"
 
 .PHONY: all clean fclean re
+
+.SILENT :
--- parser.c
+++ parser.c
@@ -1,32 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   parser.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ana-lda- <ana-lda-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/30 15:56:04 by lufiguei          #+#    #+#             */
-/*   Updated: 2024/11/01 13:27:28 by ana-lda-         ###   ########.fr       */
+/*   Created: 2024/11/01 13:27:54 by ana-lda-          #+#    #+#             */
+/*   Updated: 2024/11/01 13:28:12 by ana-lda-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
-
-# include <dirent.h>
-# include <fcntl.h>
-# include <signal.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <string.h>
-# include <stropts.h>
-# include <sys/types>
-# include <sys/stat>
-# include <sys/wait>
-# include <term.h>
-# include <termios.h>
-# include <unistd.h>
-# include <readline/history>
-# include <readline/readline>
-
-#endif
