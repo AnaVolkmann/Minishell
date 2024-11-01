fcbd077262d7ad0792167820051cc92f391f6e6f
Lucas <lufiguei@c2r1s1.42porto.com>
2024-11-01 13:41:58 +0000

    Created token structs, token append and free tokenlist

--- minishell.h
+++ minishell.h
@@ -29,4 +29,21 @@
 # include <readline/history>
 # include <readline/readline>
 
+typedef enum	e_token_type
+{
+	COMMAND,
+	RULE,
+	OPERATOR,
+	QUOTE,
+	ENV_VAR,
+	WORD
+}	t_token_type;
+
+typedef struct s_token
+{
+	t_token_type	type;
+	char		*value;
+	struct s_token	*next;
+}	t_token;
+
 #endif
---
+++ tokenizer.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tokenizer.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lufiguei <lufiguei@student.42porto.co      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/01 13:15:44 by lufiguei          #+#    #+#             */
+/*   Updated: 2024/11/01 13:17:10 by lufiguei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+t_token	*append_token(t_token **token_list, t_token_type type, char *value)
+{
+	t_token	*new_token;
+	t_token	*tmp;
+	
+	new_token = malloc(sizeof(t_token));
+	if (new_token == NULL)
+		return (NULL);
+	new_token->type = type;
+	new_token->value = ft_strdup(value);
+	if (new_token->value == NULL)
+		return (free(new_token), NULL);
+	new_token->next = NULL;
+	if (*token_list == NULL)
+		*token_list = new_token;
+	else
+	{
+		tmp = *token_list;
+		while (tmp->next != NULL)
+			tmp = tmp->next;
+		tmp->next = new_token;
+	}
+	return (new_token);
+}
+
+void	free_token_list(t_token **token_list)
+{
+	t_token	*tmp;
+	t_token	*next;
+	
+	tmp = *token_list;
+	while (tmp != NULL)
+	{
+		next = tmp->next;
+		free(tmp);
+		tmp = next;
+	}
+	*token_list = NULL;
+}
