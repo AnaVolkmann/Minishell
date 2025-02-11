6ed8f7409d69784f2513c7cf4efc4d45c22b8d5e
Lucas <lufiguei@LAPTOP-ORB35NS6.>
2025-02-11 15:55:35 +0000

    updated todo's list

--- builtin/unset.c
+++ builtin/unset.c
@@ -42,17 +42,3 @@ static void	unset_error(char *name, t_env *env, int i)
 		ft_putstr_fd("\n", 2);
 	}
 }
-
-//(TODO)
-
-// Handle ’ (single quote) which should prevent the shell from interpreting
-// the meta-characters in the quoted sequence
-
-// Handle " (double quote) which should prevent the shell from interpreting
-// the meta-characters in the quoted sequence except for $ (dollar sign).
-
-// if inside quotes, dont expand, but run command if its exactly it.
-// "ls" -l works "ls -l" no, neither "ls "-l or "ls"-l
-// pwd | $oi | cat > test
-// echo "$USER" = lufiguei
-// echo '$USER' = $USER
